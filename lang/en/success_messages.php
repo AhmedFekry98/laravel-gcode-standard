e8807f59c976b2f5a643c0c05884de114b2e23fe
abdulrhman-developer0 <tknwtknwlwjy10@gmail.com>
2024-05-27 04:16:40 +0300

    try fix bug two

---
+++ lang/en/success_messages.php
@@ -0,0 +1,12 @@
+<?php 
+
+return[ 
+    'user_register'         =>  'User Registered Successfully',
+    'user_login'            =>  "Success Login",
+    'user_logout'           =>  "Success Logout",
+    'user_forgotpassword'   =>  "Success Send Email :email_otp_forgot",
+    'user_resetpassword'    =>  "Success Reset Password",
+    'change_password'       =>  "Success Change Password",
+    'profile'               =>  "Success Get Data profile",
+    'up_profile'            =>  "Success Update Profile"
+];
