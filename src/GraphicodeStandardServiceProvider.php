0023c86dccccdbe1b87f4c383cf50f185323e4b4
abdulrhman-developer0 <tknwtknwlwjy10@gmail.com>
2024-05-27 04:03:33 +0300

    fix provider bug

---
+++ src/GraphicodeStandardServiceProvider.php
@@ -0,0 +1,18 @@
+<<?php
+
+namespace Graphicode\Standard;
+
+    use Illuminate\Support\ServiceProvider;
+
+    class GraphicodeStandardServiceProvider extends ServiceProvider
+    {
+        public function boot(): void
+        {
+            $this->publishes(
+                [
+                    __DIR__ . '/../lang' => base_path('/lang')
+                ],
+                ['gcstandard-lang']
+            );
+        }
+    }
