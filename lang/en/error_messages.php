ac0a25ae9de88dc4bb01f8c28b199704133a25e6
abdulrhman-developer0 <tknwtknwlwjy10@gmail.com>
2024-05-27 03:43:06 +0300

    fix compatiblity isue

---
+++ lang/en/error_messages.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+return [
+    'user_register'         =>   "Invalid Credentials",
+    'user_login'            =>   "Invalid Login",
+    'user_logout'           =>   "Invalid Logout",
+    'user_forgotpassword'   =>   "Invalid Email :email_otp_forgot",
+    'expird_resettoken'     =>   "Expird Token",
+    'user_resetpassword'    =>   "Invalid Reset Password",
+    'change_password'       =>   "Invalid Change Password",  
+    'profile'               =>   "Invalid Get profile" ,
+    'up_profile'            =>   "Invaild Update Profile",
+];
